017415cb570748727c2d8160648a6294a5ce11c8
Samantha Doran <samanthadoran3@gmail.com>
2016-03-15 15:47:57 -0400

    Begin fleshing out the idea of a character

--- src/character.rs
+++ src/character.rs
@@ -1,10 +1,14 @@
-use std::collections::HashMap;
+use std::collections::{HashMap, HashSet};
 use super::item::Item;
 
 
 pub struct Character {
     pub attributes: Item,
-    pub inventory: HashMap<String, (Item, i32)>
+    pub inventory: HashMap<String, (Item, i32)>,
+    pub feats: HashMap<String, HashMap<String, i32>>,
+    pub scores: HashMap<String, i32>,
+    pub ability_score_names: HashSet<String>,
+    pub equipment: HashMap<String, Item>,
 }
 
 impl Character {
@@ -12,6 +16,10 @@ impl Character {
         Character {
             attributes: Item::new(),
             inventory: HashMap::new(),
+            feats: HashMap::new(),
+            scores: HashMap::new(),
+            ability_score_names: HashSet::new(),
+            equipment: HashMap::new(),
         }
     }
 
@@ -20,5 +28,17 @@ impl Character {
         let name = item.qualitative_descriptors.get(&"name".to_string()).unwrap();
         let mut i = self.inventory.entry(name.to_string()).or_insert((item.clone(), 0));
         i.1 += count;
+    }
+
+    pub fn equip(&mut self, item: &Item) {
+        match item.qualitative_descriptors.get("slot") {
+            Some(slot) => {
+                match self.equipment.insert(slot.to_string(), item.clone()) {
+                    Some(i) => self.give_item(&i, 1),
+                    None => {},
+                }
+            },
+            None => {},
+        };
     }
 }
