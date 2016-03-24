54ebd96de6b0c80834cfc856f258c339c5f9654d
Samantha Doran <samanthadoran3@gmail.com>
2016-03-24 16:00:40 -0400

    Properly use the fields instead of static data

--- scaling-goggles.lisp
+++ scaling-goggles.lisp
@@ -4,7 +4,8 @@
   (:nicknames #:soggles)
   (:use :cl :cl-user)
   (:export #:print-pretty #:player #:make-player #:player-scores #:player-feats
-    #:player-inventory #:player-equipment #:make-feat-expression #:make-default-player))
+    #:player-inventory #:player-equipment #:make-feat-expression #:make-default-player
+    #:give-feat #:get-feat-val))
 
 (in-package :scaling-goggles)
 
@@ -23,8 +24,25 @@
       (list 10 10 10 10 10 10)))
   s))
 
-(defun make-feat-expression (score bonus class-skill player)
+(defun make-feat-expression (name bonus score class-skill player)
   "Creates a quoted expression representing a feat for a given player"
-  `(if (and ,class-skill (> ,score 0))
-    (+ ,score (cdr (assoc ,bonus (player-scores ,player) :test #'string=)) 3)
-    (+ ,score (cdr (assoc ,bonus (player-scores ,player) :test #'string=)))))
+  (pairlis (list "name" "class-skill" "bonus" "score" "value") (list name
+  class-skill bonus score
+  `(if (and
+    (cdr(assoc "class-skill" (cdr(assoc ,name (player-feats ,player) :test #'string=)) :test #'string=))
+    (>
+      (cdr (assoc "score" (cdr (assoc ,name (player-feats ,player) :test #'string=)) :test #'string=))
+      0))
+      (+
+        (cdr (assoc "score" (cdr (assoc ,name (player-feats ,player) :test #'string=)) :test #'string=))
+        (floor (- (cdr (assoc ,bonus (player-scores ,player) :test #'string=)) 10) 2)
+        3)
+      (+
+        (cdr (assoc "score" (cdr (assoc ,name (player-feats ,player) :test #'string=)) :test #'string=))
+        (floor (- (cdr (assoc ,bonus (player-scores ,player) :test #'string=)) 10) 2))))))
+
+(defun give-feat (feat player)
+  (setf (player-feats player) (acons (cdr (assoc "name" feat :test #'string=)) feat (player-feats player))))
+
+(defun get-feat-val (feat player)
+  (cdr (assoc "value" (cdr (assoc feat (player-feats player) :test #'string=)) :test #'string=)))
