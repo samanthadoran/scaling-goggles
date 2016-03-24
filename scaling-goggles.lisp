fba4b6bcedafc97748e7af8ffd06d3ce6b4acf8a
Samantha Doran <samanthadoran3@gmail.com>
2016-03-24 14:49:14 -0400

    Another helper function

--- scaling-goggles.lisp
+++ scaling-goggles.lisp
@@ -4,7 +4,7 @@
   (:nicknames #:soggles)
   (:use :cl :cl-user)
   (:export #:print-pretty #:player #:make-player #:player-scores #:player-feats
-    #:player-inventory #:player-equipment #:make-feat-expression))
+    #:player-inventory #:player-equipment #:make-feat-expression #:make-default-player))
 
 (in-package :scaling-goggles)
 
@@ -16,8 +16,15 @@
 
 (defun print-pretty ())
 
+(defun make-default-player ()
+  (let ((s (make-player)))
+    (setf (player-scores s)(pairlis
+      (list "strength" "dexterity" "constitution" "intelligence" "wisdom" "charisma")
+      (list 10 10 10 10 10 10)))
+  s))
+
 (defun make-feat-expression (score bonus class-skill player)
-  "Creates a quoted expression representing a feat for a given character"
+  "Creates a quoted expression representing a feat for a given player"
   `(if (and ,class-skill (> ,score 0))
     (+ ,score (cdr (assoc ,bonus (player-scores ,player) :test #'string=)) 3)
     (+ ,score (cdr (assoc ,bonus (player-scores ,player) :test #'string=)))))
